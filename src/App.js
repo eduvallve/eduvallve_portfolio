3d8df9c797c5fe3306e01d30a9941f149095860b
Eduard Vallvé <66818781+eduvallve@users.noreply.github.com>
2025-09-22 07:58:45 +0200

    Maintenance mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import logo from './evp-logo-white.svg';
 import './App.css';
 
 function App() {
@@ -7,17 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Lorem ipsum dolor sit amet</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          eduvallve.com is under maintenance
+        <br/>
+        <small>· We will be back soon ·</small></p>
       </header>
     </div>
   );
