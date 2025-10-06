7bf63460ce9e739747cef54cec55afc4d07d1b63
eduard.vallve.contractor <eduard.vallve.contractor@bbva.com>
2025-10-06 07:55:52 +0200

    D - Edit basename and remove github-pages settings for PROD

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Layout from './Layout';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const basename = "eduvallve_portfolio/";
+const basename = "";
 root.render(
   <React.StrictMode>
     <BrowserRouter basename={`/${basename}`} >
