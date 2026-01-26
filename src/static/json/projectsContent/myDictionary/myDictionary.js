1e37bc6d53142db9ab1ba4eb8d4e60860ebe299a
eduvallve <eduvallve@gmail.com>
2026-01-26 20:42:06 +0100

    D - Improve myDictionary contents

--- src/static/json/projectsContent/myDictionary/myDictionary.js
+++ src/static/json/projectsContent/myDictionary/myDictionary.js
@@ -20,8 +20,7 @@ export const myDictionary = [
     "@Container",
     [
       ["@HeaderBlock", "My Dictionary", { label: "h1" }],
-      "This WordPress translation plugin enables website owners to easily create and manage multilingual content without complexity. Built with performance in mind. Its intuitive interface makes it suitable for beginners, and advanced users.",
-      "This feature is still under development. Updates will be provided soon.",
+      "A fast, user-friendly WordPress plugin for effortless multilingual content management. Focused on performance and usability. Project in continuous development.",
       [
         "@TextBlock",
         "Stack: WordPress, PHP, JavaScript",
@@ -29,7 +28,7 @@ export const myDictionary = [
       ],
       [
         "@TextBlock",
-        "Developed in 2025.",
+        "Developed in <b>2025</b>",
         { className: "text-highlighted text-small" },
       ],
     ],
@@ -51,5 +50,25 @@ export const myDictionary = [
     { className: "grid-mobile-4-4 grid-desktop-10-12" },
   ],
   ["@Container", ["<br>"], { className: "grid-mobile-1-4 grid-desktop-1-12" }],
+  ["@Container", [
+    ["@HeaderBlock", "Purpose", { label: "h2" }],
+    "<b>User-friendly WordPress plugin</b> for <b>multilingual content</b>",
+    ["@ImageBlock", "", { src: AppIcon, alt: "My Carousel purpose", className: "image--boxed image--size-small image--aligned-centered" }],
+  ], { className: "grid-mobile-1-2 grid-desktop-1-3" }],
+  ["@Container", [
+    ["@ImageBlock", "", { src: AppIcon, alt: "My Carousel role", className: "image--boxed image--size-small image--aligned-centered" }],
+    ["@HeaderBlock", "My role", { label: "h2" }],
+    "<b>Lead Developer</b>: Architecture, <b>database</b> design, performance optimization and WordPress <b>hooks</b> integration",
+  ], { className: "grid-mobile-3-4 grid-desktop-4-6" }],
+  ["@Container", [
+    ["@HeaderBlock", "Challenges", { label: "h2" }],
+    "<b>Wordpress hooks</b>, database structure, <b>SEO</b> optimization",
+    ["@ImageBlock", "", { src: AppIcon, alt: "My Carousel role", className: "image--boxed image--size-small image--aligned-centered" }],
+  ], { className: "grid-mobile-1-2 grid-desktop-7-9" }],
+  ["@Container", [
+    ["@ImageBlock", "", { src: AppIcon, alt: "My Carousel role", className: "image--boxed image--size-small image--aligned-centered" }],
+    ["@HeaderBlock", "Results", { label: "h2" }],
+    "Project in development. Updates will be provided soon.",
+  ], { className: "grid-mobile-3-4 grid-desktop-10-12" }],
   ["@Container", ["<br>"], { className: "grid-mobile-1-4 grid-desktop-1-12" }],
 ];
