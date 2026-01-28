62190dd846e23a121c291b216ae06a9022c2ce03
eduvallve <eduvallve@gmail.com>
2026-01-28 14:18:22 +0100

    D - Sort projects chronollogically

--- src/static/json/portfolioList.js
+++ src/static/json/portfolioList.js
@@ -13,45 +13,45 @@ import MagiPomesRifThumbnail from "../json/projectsContent/magiPomesRif/magiPome
 
 export const projects = [
   {
-    slug: "joaprenccatala",
-    title: "Jo aprenc català",
-    description: "Website & ecommerce for an online Catalan language school",
+    slug: "icestick",
+    title: "Icestick",
+    description: "Global branding and website for an ice cream company",
     thumbnail: "#",
-    timestamp: 1476525600, // October 15, 2016
+    timestamp: 1759363200, // October 1, 2025
   },
   {
     slug: "my-carousel",
     title: "My Carousel",
     description: "Wordpress plugin for image galleries and carousels",
     thumbnail: MyCarouselThumbnail,
-    timestamp: 1759320000, // February 29, 2024
+    timestamp: 1752278400, // August 11, 2025
     content: MyCarouselContent,
     tags: ['wordpress', 'plugin', 'php', 'javascript'],
   },
   {
-    slug: "icestick",
-    title: "Icestick",
-    description: "Global branding and website for an ice cream company",
+    slug: "joaprenccatala",
+    title: "Jo aprenc català",
+    description: "Website & ecommerce for an online Catalan language school",
     thumbnail: "#",
-    timestamp: 1759320000, // February 29, 2024
-  },
-  {
-    slug: "museu-picasso-bcn",
-    title: "Museu Picasso de Barcelona",
-    description: "Fictional redesign of the website for the Picasso Museum in Barcelona",
-    thumbnail: PicassoThumbnail,
-    timestamp: 1525946400, // May 10, 2018
-    content: MuseuPicassoBcnContent,
-    tags: ['design', 'figma', 'uiux', 'javascript'],
+    timestamp: 1746835200, // July 10, 2025
   },
   {
     slug: "my-dictionary",
     title: "My Dictionary",
     description: "Wordpress plugin for multilingual sites",
     thumbnail: MyDictionaryThumbnail,
-    timestamp: 1525946400, // May 10, 2018
+    timestamp: 1730073600, // December 27, 2024
     content: MyDictionaryContent,
     tags: ['wordpress', 'plugin', 'php', 'javascript'],
+  },
+  {
+    slug: "museu-picasso-bcn",
+    title: "Museu Picasso de Barcelona",
+    description: "Fictional redesign of the website for the Picasso Museum in Barcelona",
+    thumbnail: PicassoThumbnail,
+    timestamp: 1655856000, // June 22, 2022
+    content: MuseuPicassoBcnContent,
+    tags: ['design', 'figma', 'uiux', 'javascript'],
   },
   {
     slug: "ca-les-taronges",
