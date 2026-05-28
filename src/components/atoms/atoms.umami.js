db035df271287e0e102571ba16b7ec1c7cf5100e
eduvallve <eduvallve@gmail.com>
2026-05-28 08:12:52 +0200

    D - Apply umami only if user lands out of the local admin ip

--- src/components/atoms/atoms.umami.js
+++ src/components/atoms/atoms.umami.js
@@ -1,27 +1,49 @@
 import { useEffect } from "react";
 
 const Umami = () => {
-    useEffect(() => {
-        const id = process.env.REACT_APP_UMAMI_ID;
+  const adminIp = process.env.REACT_APP_ADMIN_IP;
+  const umamiId = process.env.REACT_APP_UMAMI_ID;
 
-        if (!id) {
-            console.warn("Umami ID missing");
-            return;
+  useEffect(() => {
+    if (!umamiId) {
+      console.warn("Umami ID missing");
+      return;
+    }
+
+    let script;
+
+    fetch("https://api.ipify.org?format=json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`IP fetch failed (${response.status})`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("IP fetched:", data.ip);
+
+        if (data.ip === adminIp) {
+          console.warn("Xarxa local. NO inicialitza Umami.");
+          return;
         }
 
-        const script = document.createElement("script");
+        script = document.createElement("script");
         script.src = "https://cloud.umami.is/script.js";
         script.defer = true;
-        script.setAttribute("data-website-id", id);
+        script.setAttribute("data-website-id", umamiId);
 
         document.head.appendChild(script);
+      })
+      .catch((error) => console.error("Error al obtener la IP:", error));
 
-        return () => {
-            document.head.removeChild(script);
-        };
-    }, []);
+    return () => {
+      if (script) {
+        document.head.removeChild(script);
+      }
+    };
+  }, [adminIp, umamiId]);
 
-    return null;
+  return null;
 };
 
 export default Umami;
